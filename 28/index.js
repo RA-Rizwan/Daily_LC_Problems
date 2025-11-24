14c0fdb8d76e0606d8a2343fc3e0e1bddf3e395e
rizwan-0987 <leocoin4737@gmail.com>
2025-11-24 22:00:02 +0500

    28 solved

--- 28/index.js
+++ 28/index.js
@@ -1,3 +1,31 @@
 // Find the Index of the First Occurrence in a String
 
 // Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+function strStr(haystack, needle) {
+  const hLen = haystack.length;
+  const nLen = needle.length;
+
+  if (nLen === 0) {
+    return 0;
+  }
+
+  let i = 0;
+  while (i < hLen) {
+    let nIndex = 0;
+    let j = i;
+
+    while (j < hLen && nIndex < nLen && haystack[j] === needle[nIndex]) {
+      j++;
+      nIndex++;
+    }
+
+    if (nIndex === nLen) {
+      return i;
+    }
+
+    i++;
+  }
+
+  return -1;
+};
