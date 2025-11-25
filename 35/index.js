743f68439bfb9b6e4ae5827d5deb21b5ea6e8868
rizwan-0987 <leocoin4737@gmail.com>
2025-11-25 21:30:20 +0500

    35 solved

--- 35/index.js
+++ 35/index.js
@@ -1,3 +1,17 @@
 // Search Insert Position
 
 // Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+function searchInsert(nums, target) {
+  let lo = 0,
+    hi = nums.length;
+  while (lo < hi) {
+    let mid = lo + Math.floor((hi - lo) / 2);
+    if (target > nums[mid]) {
+      lo = mid + 1;
+    } else {
+      hi = mid;
+    }
+  }
+  return lo;
+};
