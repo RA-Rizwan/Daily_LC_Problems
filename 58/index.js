f46ea36814c8386e7f1bda8f0108b8e373b46e3a
rizwan-0987 <leocoin4737@gmail.com>
2025-11-26 19:12:54 +0500

    58 solved

--- 58/index.js
+++ 58/index.js
@@ -1,3 +1,13 @@
 // Length of Last Word
 
 // Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+ function lengthOfLastWord(s) {
+   s = s.trim();
+   let length = 0;
+   for (let i = s.length - 1; i >= 0; i--) {
+     if (s[i] !== " ") length++;
+     else if (length > 0) break;
+   }
+   return length;
+ };
