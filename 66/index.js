6d5bcacc1224b31e77f453d83b20b42a9ed4ffd6
rizwan-0987 <leocoin4737@gmail.com>
2025-11-27 23:13:31 +0500

    66 solved

--- 66/index.js
+++ 66/index.js
@@ -1,3 +1,14 @@
 // Plus One
 
 // You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+function plusOne(digits) {
+  let carry = 1;
+  for (let i = digits.length - 1; i >= 0; i--) {
+    let sum = digits[i] + carry;
+    digits[i] = sum % 10;
+    carry = Math.floor(sum / 10);
+  }
+  if (carry) digits.unshift(1);
+  return digits;
+};
