83989c1176544748276e704ef4a447b616bd72b7
rizwan-0987 <leocoin4737@gmail.com>
2025-11-28 20:13:43 +0500

    67 solved

--- 67/index.js
+++ 67/index.js
@@ -1,3 +1,10 @@
 // Add Binary
 
 // Given two binary strings a and b, return their sum as a binary string.
+
+function addBinary(a, b) {
+  const aBin = `0b${a}`;
+  const bBin = `0b${b}`;
+  const sum = BigInt(aBin) + BigInt(bBin);
+  return sum.toString(2);
+};
