33f69c99943dcba9a36f9b1c84eecc6578c85353
rizwan-0987 <leocoin4737@gmail.com>
2025-11-30 21:15:44 +0500

    125 solved

--- 125/index.js
+++ 125/index.js
@@ -2,3 +2,19 @@
 
 // A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
 // Given a string s, return true if it is a palindrome, or false otherwise.
+
+function isPalindrome(s) {
+  s = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    if (s[left] !== s[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+
+  return true;
+};
