0644be644e3287de0c5a03048900728503c01d95
rizwan-0987 <leocoin4737@gmail.com>
2025-12-01 20:54:44 +0500

    70 solved

--- 70/index.js
+++ 70/index.js
@@ -3,4 +3,18 @@
 // You are climbing a staircase. It takes n steps to reach the top.
 // Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 
- 
+ function climbStairs(n) {
+   if (n <= 3) return n;
+
+   let prev1 = 3;
+   let prev2 = 2;
+   let cur = 0;
+
+   for (let i = 3; i < n; i++) {
+     cur = prev1 + prev2;
+     prev2 = prev1;
+     prev1 = cur;
+   }
+
+   return cur;
+ };
