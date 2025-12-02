78824d799e528e1af9fa737db086027c787e778d
rizwan-0987 <leocoin4737@gmail.com>
2025-12-02 19:28:27 +0500

    217 solved

--- 217/index.js
+++ 217/index.js
@@ -1,3 +1,16 @@
 // Contains Duplicate
 
 // Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+ function containsDuplicate(nums) {
+   const numSet = new Set();
+
+   for (const n of nums) {
+     if (numSet.has(n)) {
+       return true;
+     }
+     numSet.add(n);
+   }
+
+   return false;
+ };
