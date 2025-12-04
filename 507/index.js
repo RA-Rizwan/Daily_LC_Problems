781708731374a3e9680cac692b8f51fd1eeb9a23
rizwan-0987 <leocoin4737@gmail.com>
2025-12-04 23:19:03 +0500

    507 solved

--- 507/index.js
+++ 507/index.js
@@ -3,3 +3,14 @@
 // A perfect number is a positive integer that is equal to the sum of its positive divisors, excluding the number itself. A divisor of an integer x is an integer that can divide x evenly.
 // Given an integer n, return true if n is a perfect number, otherwise return false.
 
+function checkPerfectNumber(num) {
+  if (num < 2) return false;
+  let sum = 1;
+  for (let i = 2; i * i <= num; i++) {
+    if (num % i === 0) {
+      sum += i;
+      if (i !== num / i) sum += num / i;
+    }
+  }
+  return sum === num;
+};
