c9396c39af4d63c814afa956437777ae8da3c7dc
rizwan-0987 <leocoin4737@gmail.com>
2025-12-05 20:42:46 +0500

    242 solved

--- 242/index.js
+++ 242/index.js
@@ -1,3 +1,24 @@
 // Valid Anagram
 
 // Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+function isAnagram(s, t) {
+  if (s.length !== t.length) {
+    return false;
+  }
+
+  const counter = new Map();
+
+  for (let char of s) {
+    counter.set(char, (counter.get(char) || 0) + 1);
+  }
+
+  for (let char of t) {
+    if (!counter.has(char) || counter.get(char) === 0) {
+      return false;
+    }
+    counter.set(char, counter.get(char) - 1);
+  }
+
+  return true;
+};
