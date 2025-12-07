6db0cc0b57517f19bdd2f6694887a5f0312e49bf
rizwan-0987 <leocoin4737@gmail.com>
2025-12-07 21:24:42 +0500

    709 solved

--- 709/index.js
+++ 709/index.js
@@ -1,3 +1,16 @@
 // To Lower Case
 
 // Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+function toLowerCase(s) {
+  let result = "";
+  for (let i = 0; i < s.length; i++) {
+    const ascii = s.charCodeAt(i);
+    if (ascii >= 65 && ascii <= 90) {
+      result += String.fromCharCode(ascii + 32);
+    } else {
+      result += s.charAt(i);
+    }
+  }
+  return result;
+};
