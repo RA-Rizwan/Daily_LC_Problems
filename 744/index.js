6764223d16c4182ddca9320a295119d84304a2a5
rizwan-0987 <leocoin4737@gmail.com>
2025-12-08 21:59:44 +0500

    744 solved

--- 744/index.js
+++ 744/index.js
@@ -2,3 +2,17 @@
 
 // You are given an array of characters letters that is sorted in non-decreasing order, and a character target. There are at least two different characters in letters.
 // Return the smallest character in letters that is lexicographically greater than target. If such a character does not exist, return the first character in letters.
+
+function nextGreatestLetter(letters, target) {
+  let lo = 0,
+    hi = letters.length;
+  while (lo < hi) {
+    const mid = Math.floor((lo + hi) / 2);
+    if (letters[mid] <= target) {
+      lo = mid + 1;
+    } else {
+      hi = mid;
+    }
+  }
+  return letters[lo % letters.length];
+};
