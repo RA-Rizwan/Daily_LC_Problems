346e8a72029fb0c5e5603d731894b5d9eac1e566
rizwan-0987 <leocoin4737@gmail.com>
2025-12-09 20:27:48 +0500

    746 solved

--- 746/index.js
+++ 746/index.js
@@ -4,4 +4,17 @@
 // You can either start from the step with index 0, or the step with index 1.
 // Return the minimum cost to reach the top of the floor.
 
- 
+function minCostClimbingStairs(cost) {
+  const n = cost.length;
+  let dp = 0,
+    dp1 = 0,
+    dp2 = 0;
+  for (let i = 2; i <= n; i++) {
+    const oneStep = dp1 + cost[i - 1];
+    const twoStep = dp2 + cost[i - 2];
+    dp = Math.min(oneStep, twoStep);
+    dp2 = dp1;
+    dp1 = dp;
+  }
+  return dp1;
+};
