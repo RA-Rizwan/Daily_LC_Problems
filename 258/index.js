b6d6cf50e87fc2de5a09c491fb17be3d43909fc6
rizwan-0987 <leocoin4737@gmail.com>
2025-12-10 20:39:19 +0500

    258 solved

--- 258/index.js
+++ 258/index.js
@@ -1,3 +1,7 @@
 // Add Digits
 
 // Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+
+function addDigits(num) {
+  return 1 + ((num - 1) % 9);
+};
