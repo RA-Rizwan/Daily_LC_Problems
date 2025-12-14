adde312998e3870eb8c30b42cca98a581e694ed5
rizwan-0987 <leocoin4737@gmail.com>
2025-12-14 20:10:39 +0500

    1046 solved

--- 1046/index.js
+++ 1046/index.js
@@ -1,3 +1,14 @@
 // Last Stone Weight
 
 // You are given an array of integers stones where stones[i] is the weight of the ith stone.
+
+function lastStoneWeight(stones) {
+  while (stones.length > 1) {
+    let max1 = Math.max(...stones);
+    stones.splice(stones.indexOf(max1), 1);
+    let max2 = Math.max(...stones);
+    stones.splice(stones.indexOf(max2), 1);
+    if (max1 !== max2) stones.push(Math.abs(max1 - max2));
+  }
+  return stones[0] || 0;
+};
