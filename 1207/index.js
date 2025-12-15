7d4f05b2b1adf18b39cd119dcf0ff2b2c0d4f943
rizwan-0987 <leocoin4737@gmail.com>
2025-12-15 20:44:58 +0500

    1207 solved

--- 1207/index.js
+++ 1207/index.js
@@ -1,3 +1,16 @@
 // Unique Number of Occurrences
 
 // Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.
+
+ function uniqueOccurrences(arr) {
+   const freq = new Map();
+   for (const x of arr) {
+     freq.set(x, (freq.get(x) || 0) + 1);
+   }
+
+   const s = new Set(freq.values());
+   return freq.size === s.size;
+};
+ 
+// Time complexity:O(n)
+// Space complexity:O(n)
